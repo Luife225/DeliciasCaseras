dc7734b3879131e78c333eb58340a7c991c803b4
Luife225 <Luife225@gmail.com>
2024-12-11 05:05:09 -0500

    Update sesion.js

--- html/js/sesion.js
+++ html/js/sesion.js
@@ -1,7 +1,8 @@
 document.getElementById('loginForm').addEventListener('submit', function(e) {
     e.preventDefault();
     const modal = document.getElementById('modalLogin');
-    modal.style.display = 'flex';
+    modal.style.display = 'flex'; 
+
     const cerrarModal = document.getElementById('cerrarModalLogin');
     cerrarModal.addEventListener('click', function() {
         modal.style.display = 'none';
