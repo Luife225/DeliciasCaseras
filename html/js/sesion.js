ccbe1a08854143408710c65fad83abfc7cdb6ea5
Luife225 <Luife225@gmail.com>
2024-12-11 04:53:52 -0500

    Update sesion.js

--- html/js/sesion.js
+++ html/js/sesion.js
@@ -12,18 +12,16 @@ document.querySelector('.loginForm').addEventListener('submit', function (e) {
 
     const userData = JSON.parse(storedData);
 
-    if (loginUsuario === userData.nombreUsuario && loginContrasena === userData.contrasena) {
-        // Marcar al usuario como autenticado
+    if (loginUsuario === userData.usuario && loginContrasena === userData.contrasena) {
         localStorage.setItem('isLoggedIn', 'true');
 
-        // Mostrar el modal de inicio de sesión exitoso
         const modal = document.getElementById('modalLogin');
-        modal.style.display = 'flex';
+        modal.style.display = 'flex'; 
 
         const cerrarModal = document.getElementById('cerrarModalLogin');
         cerrarModal.addEventListener('click', () => {
             modal.style.display = 'none';
-            window.location.href = 'indexDespues.html'; // Redirige a la página principal
+            window.location.href = 'indexDespues.html'; 
         });
     } else {
         alert('Usuario o contraseña incorrectos.');
