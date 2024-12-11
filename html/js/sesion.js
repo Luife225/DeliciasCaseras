ed462e44a837f6b7d2cb6a0ffbb0f33812773ad9
Luife225 <Luife225@gmail.com>
2024-12-11 05:11:49 -0500

    Update sesion.js

--- html/js/sesion.js
+++ html/js/sesion.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('loginForm').addEventListener('submit', function(e) {
         e.preventDefault();
-        console.log('Formulario enviado');
-
+        
         const modal = document.getElementById('modalLogin');
         modal.style.display = 'flex';
 
         const cerrarModal = document.getElementById('cerrarModalLogin');
         cerrarModal.addEventListener('click', function() {
             modal.style.display = 'none';
+            window.location.href = 'indexDespues.html';
         });
     });
 });
